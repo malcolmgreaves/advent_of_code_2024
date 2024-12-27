9d52beea98dfe762c5ae5e6ea2d9926a4befa620
Malcolm Greaves <greaves.malcolm@gmail.com>
2024-12-27 12:21:17 -0500

    finished AOC #2

--- src/aoc_2.rs
+++ src/aoc_2.rs
@@ -15,23 +15,29 @@ pub fn solution() -> i32 {
         [8, 6, 4, 4, 1,],
         [1, 3, 6, 7, 9,],
     ];
+
     levels.map(|level| check_level(&level)).map(|x| if x { 1 } else { 0 }).iter().sum()
 }
 
 fn check_level(level: &[i32]) -> bool {
     // True iff there are at least 2 elements, they're all ascending or descending, and the diff between any two is in [1,3]. 
-
     if level.len() < 2 {
         return false
     }
 
     // The levels are either all increasing or all decreasing.
-    let ascending_or_descending: Vec<i32> = level.windows(2)
+    let diffs: Vec<i32> = level.windows(2)
         .map(|w| {
             let (a,b) = (w[0], w[1]);
             a-b
         })
         .collect();
+
+    let is_descending = diffs.iter().all(|x| x >= &0);
+    // println!("is_descending: {is_descending:?}");
+    let is_ascending = diffs.iter().all(|x| x <= &0);
+    // println!("is_ascending: {is_ascending:?}");
+    let ascending_or_descending = is_ascending || is_descending;
         
     // level[1..level.len()].iter().fold(level[0], |prev, x| prev - x);
     
@@ -42,10 +48,11 @@ fn check_level(level: &[i32]) -> bool {
         .map(|w| {
             let (a,b) = (w[0], w[1]);
             let diff = if a < b { b - a } else { a - b };
-            let ok_diff = diff < 1 || diff > 3;
+            let ok_diff = diff >= 1 && diff <= 3;
             ok_diff
         })
         .fold(true, |acc, x| acc && x);
 
-    return ascending_or_descending==0 && diff_check;
+    // println!("diff_check: {diff_check:?}");
+    return ascending_or_descending && diff_check;
 }
