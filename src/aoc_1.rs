e478550dbe311df52cd6529679c26b2bb4828879
Malcolm Greaves <greaves.malcolm@gmail.com>
2024-12-29 18:27:34 -0600

    AoC #1 done, #2 needs file I/O. Unit tests implemented; inputs/{1,2} has data

--- src/aoc_1.rs
+++ src/aoc_1.rs
@@ -1,6 +1,23 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 pub fn solution() -> i32 {
     // https://adventofcode.com/2024/day/1
-    sum_sorted_distances(&[3,4,2,1,3,3], &[4,3,5,3,9,3])
+    let f = File::open("./inputs/1").expect("Could not load file!");
+    let reader = BufReader::new(f);
+    let (l1, l2) = reader
+        .lines()
+        .map(|l| {
+            let l = l.expect("Could not read a line of text.");
+            let bits = l.trim().split("   ").collect::<Vec<&str>>();
+            (to_int(bits[0]), to_int(bits[1]))
+        })
+        .unzip::<i32, i32, Vec<i32>, Vec<i32>>();
+    sum_sorted_distances(&l1, &l2)
+}
+
+fn to_int(s: &str) -> i32 {
+    s.parse::<i32>().expect(format!("Could not convert to i32! Original: '{s}'").as_str())
 }
 
 fn sum_sorted_distances(list1: &[i32], list2: &[i32]) -> i32 {
@@ -8,5 +25,20 @@ fn sum_sorted_distances(list1: &[i32], list2: &[i32]) -> i32 {
     list1.sort();
     let mut list2 = list2.to_vec();
     list2.sort();
-    list1.iter().zip(list2.iter()).map(|(l1,l2)| if l1 < l2 { l2 - l1 } else { l1 - l2 }).sum()
+    list1
+        .iter()
+        .zip(list2.iter())
+        .map(|(l1, l2)| if l1 < l2 { l2 - l1 } else { l1 - l2 })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_solution() {
+        let result = sum_sorted_distances(&[3, 4, 2, 1, 3, 3], &[4, 3, 5, 3, 9, 3]);
+        assert_eq!(result, 11);
+    }
 }
--- src/aoc_2.rs
+++ src/aoc_2.rs
@@ -7,29 +7,22 @@
 
 pub fn solution() -> i32 {
     // https://adventofcode.com/2024/day/2
-    let levels = [
-        [7, 6, 4, 2, 1,],
-        [1, 2, 7, 8, 9,],
-        [9, 7, 6, 2, 1,],
-        [1, 3, 2, 4, 5,],
-        [8, 6, 4, 4, 1,],
-        [1, 3, 6, 7, 9,],
-    ];
-
-    levels.map(|level| check_level(&level)).map(|x| if x { 1 } else { 0 }).iter().sum()
+    println!("ERROR: need to implement file reading from inputs/2");
+    -1
 }
 
 fn check_level(level: &[i32]) -> bool {
-    // True iff there are at least 2 elements, they're all ascending or descending, and the diff between any two is in [1,3]. 
+    // True iff there are at least 2 elements, they're all ascending or descending, and the diff between any two is in [1,3].
     if level.len() < 2 {
-        return false
+        return false;
     }
 
     // The levels are either all increasing or all decreasing.
-    let diffs: Vec<i32> = level.windows(2)
+    let diffs: Vec<i32> = level
+        .windows(2)
         .map(|w| {
-            let (a,b) = (w[0], w[1]);
-            a-b
+            let (a, b) = (w[0], w[1]);
+            a - b
         })
         .collect();
 
@@ -38,15 +31,14 @@ fn check_level(level: &[i32]) -> bool {
     let is_ascending = diffs.iter().all(|x| x <= &0);
     // println!("is_ascending: {is_ascending:?}");
     let ascending_or_descending = is_ascending || is_descending;
-        
+
     // level[1..level.len()].iter().fold(level[0], |prev, x| prev - x);
-    
 
     // Any two adjacent levels differ by at least one and at most three.
     let diff_check = level
         .windows(2)
         .map(|w| {
-            let (a,b) = (w[0], w[1]);
+            let (a, b) = (w[0], w[1]);
             let diff = if a < b { b - a } else { a - b };
             let ok_diff = diff >= 1 && diff <= 3;
             ok_diff
@@ -55,4 +47,28 @@ fn check_level(level: &[i32]) -> bool {
 
     // println!("diff_check: {diff_check:?}");
     return ascending_or_descending && diff_check;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example_solution() {
+        let levels = [
+            [7, 6, 4, 2, 1],
+            [1, 2, 7, 8, 9],
+            [9, 7, 6, 2, 1],
+            [1, 3, 2, 4, 5],
+            [8, 6, 4, 4, 1],
+            [1, 3, 6, 7, 9],
+        ];
+
+        let result = levels
+            .map(|level| check_level(&level))
+            .map(|x| if x { 1 } else { 0 })
+            .iter()
+            .sum::<i32>();
+        assert!(result == 2);
+    }
 }
