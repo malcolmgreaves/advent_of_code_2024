120d5b0e709272e1e30b302c4cf2b559f0a137a9
Malcolm Greaves <greaves.malcolm@gmail.com>
2025-02-07 20:08:29 -0500

    Add template for new AoC solutions

---
+++ TEMPLATE_AOC_SOLUTION.rs
@@ -0,0 +1,52 @@
+use crate::io_help;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn solution_pt1() -> u64 {
+    let lines = io_help::read_lines("./inputs/10").collect::<Vec<String>>();
+    panic!();
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn solution_pt2() -> u64 {
+    let lines = io_help::read_lines("./inputs/10").collect::<Vec<String>>();
+    panic!();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod test {
+
+    use indoc::indoc;
+    use lazy_static::lazy_static;
+
+    use crate::io_help::read_lines_in_memory;
+
+    use super::*;
+
+    const EXAMPLE_INPUT_STR: &str = indoc! {"
+    "};
+
+    lazy_static! {
+        static ref EXAMPLE_EXPECTED = None
+    }
+
+    #[test]
+    fn construct() {
+        panic!();
+    }
+
+    #[test]
+    fn pt1_soln_example() {
+        panic!();
+    }
+
+    #[test]
+    fn pt2_soln_example() {
+        panic!();
+    }
+}
+
