98640683b8089d5c49f6edabdd4ee4a563fac275
Malcolm Greaves <greaves.malcolm@gmail.com>
2025-02-11 17:20:14 -0500

    wip | implemented construction

--- src/aoc_12.rs
+++ src/aoc_12.rs
@@ -1,17 +1,25 @@
-use crate::io_help;
+use crate::{io_help, utils::Matrix};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub fn solution_pt1() -> u64 {
     let lines = io_help::read_lines("./inputs/12").collect::<Vec<String>>();
+    let garden = construct(&lines);
+    panic!();
+}
+
+pub fn solution_pt2() -> u64 {
+    let lines = io_help::read_lines("./inputs/12").collect::<Vec<String>>();
+    let garden = construct(&lines);
     panic!();
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub fn solution_pt2() -> u64 {
-    let lines = io_help::read_lines("./inputs/12").collect::<Vec<String>>();
-    panic!();
+type Garden = Matrix<char>;
+
+fn construct(lines: &[String]) -> Garden {
+    lines.iter().map(|l| l.chars().collect()).collect()
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -26,16 +34,79 @@ mod test {
 
     use super::*;
 
-    const EXAMPLE_INPUT_STR: &str = indoc! {"
+    const EXAMPLE_INPUT_STR_SM: &str = indoc! {"
+        AAAA
+        BBCD
+        BBCC
+        EEEC
+    "};
+
+    const EXAMPLE_INPUT_STR_2P: &str = indoc! {"
+        OOOOO
+        OXOXO
+        OOOOO
+        OXOXO
+        OOOOO
+    "};
+
+    const EXAMPLE_INPUT_STR_LG: &str = indoc! {"
+        RRRRIICCFF
+        RRRRIICCCF
+        VVRRRCCFFF
+        VVRCCCJFFF
+        VVVVCJJCFE
+        VVIVCCJJEE
+        VVIIICJJEE
+        MIIIIIJJEE
+        MIIISIJEEE
+        MMMISSJEEE
     "};
 
     lazy_static! {
-        static ref EXAMPLE_EXPECTED: Option<u8> = None;
+        static ref EXAMPLE_SM: Garden = vec![
+            vec!['A', 'A', 'A', 'A'],
+            vec!['B', 'B', 'C', 'D'],
+            vec!['B', 'B', 'C', 'C'],
+            vec!['E', 'E', 'E', 'C'],
+        ];
+        static ref EXAMPLE_2P: Garden = vec![
+            vec!['O', 'O', 'O', 'O', 'O'],
+            vec!['O', 'X', 'O', 'X', 'O'],
+            vec!['O', 'O', 'O', 'O', 'O'],
+            vec!['O', 'X', 'O', 'X', 'O'],
+            vec!['O', 'O', 'O', 'O', 'O'],
+        ];
+        static ref EXAMPLE_LG: Garden = vec![
+            vec!['R', 'R', 'R', 'R', 'I', 'I', 'C', 'C', 'F', 'F'],
+            vec!['R', 'R', 'R', 'R', 'I', 'I', 'C', 'C', 'C', 'F'],
+            vec!['V', 'V', 'R', 'R', 'R', 'C', 'C', 'F', 'F', 'F'],
+            vec!['V', 'V', 'R', 'C', 'C', 'C', 'J', 'F', 'F', 'F'],
+            vec!['V', 'V', 'V', 'V', 'C', 'J', 'J', 'C', 'F', 'E'],
+            vec!['V', 'V', 'I', 'V', 'C', 'C', 'J', 'J', 'E', 'E'],
+            vec!['V', 'V', 'I', 'I', 'I', 'C', 'J', 'J', 'E', 'E'],
+            vec!['M', 'I', 'I', 'I', 'I', 'I', 'J', 'J', 'E', 'E'],
+            vec!['M', 'I', 'I', 'I', 'S', 'I', 'J', 'E', 'E', 'E'],
+            vec!['M', 'M', 'M', 'I', 'S', 'S', 'J', 'E', 'E', 'E'],
+        ];
+    }
+
+    fn into_lines(contents: &str) -> Vec<String> {
+        read_lines_in_memory(contents).collect::<Vec<_>>()
     }
 
     #[test]
     fn construction() {
-        panic!();
+        let actual = construct(&into_lines(EXAMPLE_INPUT_STR_SM));
+        let expected: &Garden = &EXAMPLE_SM;
+        assert_eq!(actual, *expected);
+        //
+        let actual = construct(&into_lines(EXAMPLE_INPUT_STR_2P));
+        let expected: &Garden = &EXAMPLE_2P;
+        assert_eq!(actual, *expected);
+        //
+        let actual = construct(&into_lines(EXAMPLE_INPUT_STR_LG));
+        let expected: &Garden = &EXAMPLE_LG;
+        assert_eq!(actual, *expected);
     }
 
     #[test]
