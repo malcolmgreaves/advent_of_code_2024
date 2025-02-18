44dc3f24e1a51c983b5152cb39e7f6a3e0dc03a7
Malcolm Greaves <greaves.malcolm@gmail.com>
2025-02-18 14:12:28 -0500

    simplified: put prints into main()

--- y.rs
+++ y.rs
@@ -2,17 +2,17 @@ fn foo() -> impl Fn(i32) -> i32 {
     |x| x + 1
 }
 
-fn bar(f: &impl Fn(i32) -> i32) {
-   println!("from bar: f(10): {}", f(10));
+fn bar(f: &impl Fn(i32) -> i32) -> i32 {
+   f(10)
 }
 
-fn baz(f: &dyn Fn(i32) -> i32) {
-  println!("from baz: f(10): {}", f(10));
+fn baz(f: &dyn Fn(i32) -> i32) -> i32 {
+  f(10)
 }
 
 pub fn main() {
   let f = foo();
-  println!("foo()(10): {}", f(10));
-  bar(&f);
-  baz(&f);
+  println!("foo()(10):       {}", f(10));
+  println!("from bar: f(10): {}", bar(&f));
+  println!("from baz: f(10): {}", baz(&f));
 }
