6235d57958af9f717b9a53d6da327218873087df
Malcolm Greaves <greaves.malcolm@gmail.com>
2025-02-18 14:01:35 -0500

    exploration: different assembly code for impl vs dyn !

---
+++ y.rs
@@ -0,0 +1,18 @@
+fn foo() -> impl Fn(i32) -> i32 {
+    |x| x + 1
+}
+
+fn bar(f: &impl Fn(i32) -> i32) {
+   println!("from bar: f(10): {}", f(10));
+}
+
+fn baz(f: &dyn Fn(i32) -> i32) {
+  println!("from baz: f(10): {}", f(10));
+}
+
+pub fn main() {
+  let f = foo();
+  println!("foo()(10): {}", f(10));
+  bar(&f);
+  baz(&f);
+}
