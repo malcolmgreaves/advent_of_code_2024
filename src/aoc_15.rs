48f6680a33af41e1c0d4455f6b5f52fc470710be
Malcolm Greaves <greaves.malcolm@gmail.com>
2025-03-13 14:45:51 -0400

    started #15 from template

---
+++ src/aoc_15.rs
@@ -0,0 +1,66 @@
+use crate::io_help;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct TODO {}
+
+fn construct(lines: &[String]) -> Result<TODO, String> {
+    panic!()
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn solution_pt1() -> u64 {
+    let lines = io_help::read_lines("./inputs/???").collect::<Vec<String>>();
+    panic!("{} lines", lines.len());
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub fn solution_pt2() -> u64 {
+    let lines = io_help::read_lines("./inputs/???").collect::<Vec<String>>();
+    panic!("{} lines", lines.len());
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[cfg(test)]
+mod test {
+
+    use indoc::indoc;
+    use lazy_static::lazy_static;
+
+    use crate::io_help::read_lines_in_memory;
+
+    use super::*;
+
+    ///////////////////////////////////////////////
+
+    const EXAMPLE_INPUT_STR: &str = indoc! {"
+    "};
+
+    lazy_static! {
+        static ref EXAMPLE_EXPECTED: Option<u8> = None;
+    }
+
+    ///////////////////////////////////////////////
+
+    #[test]
+    fn construction() {
+        panic!();
+    }
+
+    ///////////////////////////////////////////////
+
+    #[test]
+    fn pt1_soln_example() {
+        panic!();
+    }
+
+    ///////////////////////////////////////////////
+
+    #[test]
+    fn pt2_soln_example() {
+        panic!();
+    }
+}
