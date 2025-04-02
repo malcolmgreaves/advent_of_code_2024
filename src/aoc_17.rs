95ed847d94dc799ea8033c53e9b909c99958f78d
Malcolm Greaves <greaves.malcolm@gmail.com>
2025-04-01 20:26:18 -0400

    wip

--- src/aoc_17.rs
+++ src/aoc_17.rs
@@ -1,12 +1,223 @@
-use crate::io_help;
+use std::fmt::format;
+
+use crate::{io_help, utils::collect_results};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-struct TODO {}
+type Register = i32;
 
-fn construct(lines: impl Iterator<Item = String>) -> Result<TODO, String> {
-    panic!()
+#[allow(non_snake_case)]
+struct Computer {
+    A: Register,
+    B: Register,
+    C: Register,
 }
+
+type Opcode = u8;
+type Operand = u8;
+
+type RawProgram = Vec<(Opcode, Operand)>;
+type Program = Vec<Instruction>;
+
+struct Executable {
+    pc: usize,
+    program: Program,
+}
+
+impl Executable {
+    pub fn increment(mut self) {
+        self.pc += 2;
+    }
+
+    pub fn jump(mut self, instruction_pointer: usize) {
+        self.pc = instruction_pointer;
+    }
+
+    /// None if program has halted.
+    /// This occurs if the instruction pointer is past the end of the program.
+    pub fn instruction(&self) -> Option<&Instruction> {
+        if self.pc >= self.program.len() {
+            None
+            // return Err(format!("HALT: Execution reached end of program. Instruction pointer: {} | Program length: {}", self.pc, self.program.len()));
+        } else {
+            Some(&self.program[self.pc])
+        }
+    }
+}
+
+#[allow(non_camel_case_types)]
+enum Instruction {
+    adv(Operand),
+    bxl(Operand),
+    bst(Operand),
+    jnz(Operand),
+    bxc,
+    out(Operand),
+    bdv(Operand),
+    cdv(Operand),
+}
+
+impl Instruction {
+    fn new(opcode: Opcode, operand: Operand) -> Result<Instruction, String> {
+        match opcode {
+            0 => Ok(Self::adv(operand)),
+            1 => Ok(Self::bxl(operand)),
+            2 => Ok(Self::bst(operand)),
+            3 => Ok(Self::jnz(operand)),
+            4 => Ok(Self::bxc), // ignores operand: only there for "legacy" reasons :)
+            5 => Ok(Self::out(operand)),
+            6 => Ok(Self::bdv(operand)),
+            7 => Ok(Self::cdv(operand)),
+            _ => Err(format!("unrecognized opcode: {opcode} -- must be in [0,7]")),
+        }
+    }
+
+    fn opcode(&self) -> Opcode {
+        match self {
+            Self::adv(_) => 0,
+            Self::bxl(_) => 1,
+            Self::bst(_) => 2,
+            Self::jnz(_) => 3,
+            Self::bxc => 4,
+            Self::out(_) => 5,
+            Self::bdv(_) => 6,
+            Self::cdv(_) => 7,
+        }
+    }
+}
+
+fn compile(program: RawProgram) -> Result<Program, String> {
+    let (instructions, errors) = collect_results(
+        program
+            .into_iter()
+            .map(|(opcode, operand)| Instruction::new(opcode, operand)),
+    );
+    if errors.len() > 0 {
+        Err(format!(
+            "Failed to parse {} (opcode,operand)s into instructions:\n\t{}",
+            errors.len(),
+            errors.join("\n\t")
+        ))
+    } else {
+        Ok(instructions)
+    }
+}
+
+fn construct(mut lines: impl Iterator<Item = String>) -> Result<(Computer, Program), String> {
+    let mut program_line = None;
+
+    let register_lines = [
+        match lines.next() {
+            Some(l) => l,
+            None => {
+                return Err(format!(
+                    "expecting first line to be Register A but iterator is empty"
+                ));
+            }
+        },
+        match lines.next() {
+            Some(l) => l,
+            None => {
+                return Err(format!(
+                    "expecting second line to be Register B but iterator is empty"
+                ));
+            }
+        },
+        match lines.next() {
+            Some(l) => l,
+            None => {
+                return Err(format!(
+                    "expecting third line to be Register C but iterator is empty"
+                ));
+            }
+        },
+    ];
+
+    let computer = parse_computer(&register_lines)?;
+
+    // ingore the next line -> is a blank line
+    _ = lines.next();
+
+    let raw = match lines.next() {
+        Some(program_line) => parse_raw_program(program_line),
+        None => Err(format!(
+            "expecting last line to be program line, but iterator is empty"
+        )),
+    }?;
+    let program = compile(raw)?;
+
+    Ok((computer, program))
+}
+
+fn parse_computer(lines: [String; 3]) -> Result<Computer, String> {
+    let (mut registers, errors) = collect_results(
+        lines
+            .map(|line| {
+                let mut bits = line.split(": ").collect::<Vec<_>>();
+                if bits.len() != 2 {
+                    Err(format!("Expecting each register to list its label and value with ':'. Failed to parse: {line}"))
+                } else {
+                    bits.swap_remove(1).parse::<i32>().map_err(|e| format!("{e}"))
+                }
+            })
+            .into_iter()
+    );
+    #[allow(non_snake_case)]
+    if errors.len() > 0 {
+        Err(format!(
+            "found {} errors when parsing register lines:\n\t{}",
+            errors.len(),
+            errors.join("\n\t")
+        ))
+    } else {
+        assert!(registers.len() == 3);
+        let C = registers.remove(2);
+        let B = registers.remove(1);
+        let A = registers.remove(0);
+        Ok(Computer { A, B, C })
+    }
+}
+
+fn parse_raw_program(line: String) -> Result<RawProgram, String> {
+    let mut bits = line.split("Program: ").collect::<Vec<_>>();
+    let (ops, errors) = collect_results(
+        bits.swap_remove(1)
+            .split(",")
+            .map(|x| x.parse::<u8>().map_err(|e| format!("{e}"))),
+    );
+    if errors.len() > 0 {
+        Err(format!(
+            "encountered {} errors when parsing raw program's opcode & operand pairs:\n\t",
+            errors.len(),
+            errors.join("\n\t")
+        ))
+    } else {
+        if ops.len() % 2 != 0 {
+            Err(format!(
+                "every opcode must have one operand -- found an odd number of ops: {}",
+                ops.len()
+            ))
+        } else {
+            let raw = ops.chunks_exact(2);
+            assert_eq!(raw.remainder().len(), 0);
+            Ok(raw
+                .into_iter()
+                .map(|chunk_2| {
+                    assert_eq!(chunk_2.len(), 2);
+                    (chunk_2[0], chunk_2[1])
+                })
+                .collect())
+        }
+    }
+}
+
+/*
+Register A: 18427963
+Register B: 0
+Register C: 0
+
+Program: 2,4,1,1,7,5,0,3,4,3,1,6,5,5,3,0
+ */
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
