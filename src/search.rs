14c66ef3ba5d05b491e62c5e9fecd7094c7155d8
Malcolm Greaves <greaves.malcolm@gmail.com>
2025-04-05 22:11:13 -0400

    wip

--- src/search.rs
+++ src/search.rs
@@ -72,6 +72,15 @@ pub fn binary_search_range_on_answer<N: Numeric>(
     // is_found: impl Fn(N) -> bool,
     compare: impl Fn(N) -> Ordering,
 ) -> (N, N) {
+    binary_search_range_on_answer_2(low, high, compare)
+}
+
+pub fn binary_search_range_on_answer_1<N: Numeric>(
+    low: N,
+    high: N,
+    // is_found: impl Fn(N) -> bool,
+    compare: impl Fn(N) -> Ordering,
+) -> (N, N) {
     assert!(
         low < high,
         "FATAL: must ensure that low < high | low={low:?}, high={high:?}"
@@ -134,7 +143,133 @@ pub fn binary_search_range_on_answer<N: Numeric>(
     // }
 }
 
-// pub fn binary_search_range_on_answer_2<N: Numeric>(
+pub fn binary_search_range_on_answer_2<N: Numeric>(
+    low: N,
+    high: N,
+    a_cmp_b: impl Fn(N) -> Ordering,
+) -> (N, N) {
+    assert!(
+        low < high,
+        "FATAL: must ensure that low < high | low={low:?}, high={high:?}"
+    );
+    let initial_low_bound = low;
+    let initial_high_bound = high;
+    let two = N::one() + N::one();
+
+    let mut low = initial_low_bound;
+    let mut high = initial_high_bound;
+
+    let mut high_range = None;
+    while plus_one(&low) < high {
+        let midpoint = low + (high.checked_sub(&low).unwrap() / two);
+
+        match a_cmp_b(midpoint) {
+            Ordering::Greater => {
+                // reference is LESS than midpoint
+                high = midpoint;
+                println!("\t[find high] <: [{low:?}, {high:?}]");
+            }
+            Ordering::Less => {
+                low = midpoint;
+                println!("\t[find high] > [{low:?}, {high:?}]");
+            }
+            Ordering::Equal => {
+                high_range = Some(midpoint);
+                // keep going -> what's the TOP of this equal range?
+                low = midpoint;
+                println!("\t[find high] = [{low:?}, {high:?}]");
+            }
+        }
+    }
+    let final_high = {
+        let h = match high_range {
+            Some(h) => h,
+            None => low,
+        };
+        let h_m_1 = h.checked_sub(&N::one()).unwrap_or(h);
+        let h_p_1 = h + N::one();
+        match (a_cmp_b(h_m_1), a_cmp_b(h), a_cmp_b(h_p_1)) {
+            (_, _, Ordering::Equal) => h_p_1,
+            (_, Ordering::Equal, _) => h,
+            _ => h_m_1,
+        }
+    };
+    println!("\t[find high] STOP: [{low:?}, {high:?}] high_range={final_high:?}");
+
+    low = initial_low_bound;
+    high = initial_high_bound;
+    let mut low_range = None;
+    while plus_one(&low) < high {
+        let midpoint = low + (high.checked_sub(&low).unwrap() / two);
+        match a_cmp_b(midpoint) {
+            Ordering::Less => {
+                // reference is GREATER than midpoint
+                low = midpoint;
+                println!("\t[find low] >: [{low:?}, {high:?}]");
+            }
+            Ordering::Equal => {
+                low_range = Some(midpoint);
+                // keep going -> what's the BOTTOM of this equal range?
+                high = midpoint;
+                println!("\t[find low] = [{low:?}, {high:?}]");
+            }
+            Ordering::Greater => {
+                high = midpoint;
+                println!("\t[find low] < [{low:?}, {high:?}]");
+            }
+        }
+    }
+    let final_low = {
+        let l = match low_range {
+            Some(l) => l,
+            None => low,
+        };
+
+        if l == N::zero() {
+            if a_cmp_b(l) == Ordering::Equal {
+                l
+            } else {
+                let l_p_1 = l + N::one();
+                if a_cmp_b(l_p_1) == Ordering::Equal {
+                    l_p_1
+                } else {
+                    panic!("WAT")
+                }
+            }
+        } else {
+            let l_m_1 = l - N::one();
+            let l_p_1 = l + N::one();
+            match (a_cmp_b(l_m_1), a_cmp_b(l), a_cmp_b(l_p_1)) {
+                (Ordering::Equal, _, _) => l_m_1,
+                (_, Ordering::Equal, _) => l,
+                _ => l_p_1,
+            }
+        }
+    };
+    println!("\t[find low] STOP: [{low:?}, {high:?}] low_range={final_low:?}");
+
+    (final_low, final_high)
+
+    // if plus_one(&low_range) >= high_range {
+    //     if a_cmp_b(low_range) == Ordering::Equal {
+    //         let ans = low_range;
+    //         println!("\t[STOP-CHK] answer: [{ans:?},{ans:?}]");
+    //         (ans, ans)
+    //     } else if a_cmp_b(high_range) == Ordering::Equal {
+    //         let ans = high_range;
+    //         println!("\t[STOP-CHK] answer: [{ans:?},{ans:?}]");
+    //         (ans, ans)
+    //     } else {
+    //         println!("\t[STOP-ERR] answer: [{initial_low_bound:?}, {initial_high_bound:?}]");
+    //         (initial_low_bound, initial_high_bound)
+    //     }
+    // } else {
+    //     println!("\t[STOP-FND] answer: [{low_range:?}, {high_range:?}]");
+    //     (low_range, high_range)
+    // }
+}
+
+// pub fn binary_search_range_on_answer_3<N: Numeric>(
 //     low: N,
 //     high: N,
 //     compare: impl Fn(N) -> Ordering,
@@ -304,7 +439,6 @@ mod test {
     fn range_first() {
         // single element, first
         let (low, high) = binary_search_range_on_answer(0, 10, compare(0));
-        // let (low, high) = binary_search_range_on_answer_2(0, 10, compare(0));
         println!("low={low} | high={high}");
         assert_eq!((low, high), (0, 0));
     }
@@ -313,7 +447,6 @@ mod test {
     fn range_last() {
         // single element, last
         let (low, high) = binary_search_range_on_answer(0, 10, compare(10));
-        // let (low, high) = binary_search_range_on_answer_2(0, 10, compare(10));
         println!("low={low} | high={high}");
         assert_eq!((low, high), (10, 10));
     }
@@ -322,7 +455,6 @@ mod test {
     fn range_single() {
         // single element, middle
         let (low, high) = binary_search_range_on_answer(0, 10, compare(5));
-        // let (low, high) = binary_search_range_on_answer_2(0, 10, compare(5));
         println!("low={low} | high={high}");
         assert_eq!((low, high), (5, 5));
     }
@@ -330,7 +462,6 @@ mod test {
     #[test]
     fn range_middle() {
         let (low, high) = binary_search_range_on_answer(0, 10, compare_range(3, 7));
-        // let (low, high) = binary_search_range_on_answer_2(0, 10, compare_range(3, 7));
         println!("low={low} | high={high}");
         assert_eq!((low, high), (3, 7));
     }
@@ -338,7 +469,6 @@ mod test {
     #[test]
     fn range_all() {
         let (low, high) = binary_search_range_on_answer(0, 10, compare_range(0, 10));
-        // let (low, high) = binary_search_range_on_answer_2(0, 10, compare_range(0, 10));
         println!("low={low} | high={high}");
         assert_eq!((low, high), (0, 10));
     }
@@ -346,7 +476,6 @@ mod test {
     #[test]
     fn range_low_to_middle() {
         let (low, high) = binary_search_range_on_answer(0, 10, compare_range(0, 4));
-        // let (low, high) = binary_search_range_on_answer_2(0, 10, compare_range(0, 10));
         println!("low={low} | high={high}");
         assert_eq!((low, high), (0, 4));
     }
@@ -354,7 +483,6 @@ mod test {
     #[test]
     fn range_middle_to_high() {
         let (low, high) = binary_search_range_on_answer(0, 10, compare_range(2, 10));
-        // let (low, high) = binary_search_range_on_answer_2(0, 10, compare_range(0, 10));
         println!("low={low} | high={high}");
         assert_eq!((low, high), (2, 10));
     }
